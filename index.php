9d1bbaf0d173fec26104c0a5de640d07cd5cecdb
hregnie2 <hregnie2@adm-info03>
2013-02-21 17:26:09 +0100

    Premier commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="custom.css" rel="stylesheet" type="text/css"/>
+		<link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+		<link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet" type="text/css"/>
+		
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<header class="span12">header</header>
+				<aside class="span1">
+					<div class="row">
+						<div class="span1">adise 1</div>
+						<div class="span1">aside 2</div>
+					</div>
+				</aside>
+				<section class="span11">
+					<div class="span2 no-margin"><img src="http://fakeimg.pl/170x100/?text=Illustration"/></div>
+					<div class="span9">Bla</div>
+				</section>
+				<footer class="span12">footer</footer>
+			</div>
+		</div>
+		
+		<script src="jquery.js"></script>
+		<script src="bootstrap/js/bootstrap.js"></script>
+	</body>
+</html>
